622950ff1f52400e9239ce8686cda8d297542c2d
Psyj1 <seiji.paulo@proton.me>
2025-04-06 15:16:43 -0300

    uncompleted(again...)

---
+++ app.js
@@ -0,0 +1,16 @@
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = [];
+
+todoForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo(){
+    const todoText = todoInput.value.trim();
+    allTodos.push(todoText);
+    console.log(allTodos);
+}    
